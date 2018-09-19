2e11c32f11df4d1e2cbf346727c0a220ee2a96a9
oalberto96 <ochoapublic@gmail.com>
2018-09-19 12:57:19 -0700

    Init react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">asd</div>;
+  }
+}
+
+export default App;
