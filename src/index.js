e6b27efb4f20ca50cccbc758315598b7c62b7465
oalberto96 <ochoapublic@gmail.com>
2018-09-19 14:17:38 -0700

    Add initial state

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import configureStore from "./store";
+import initialState from "./store/initialState";
 
-const store = configureStore();
+const store = configureStore(initialState);
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/store/initialState.js
@@ -0,0 +1,26 @@
+export default {
+  lists: {
+    1: {
+      name: "name",
+      description: "little description",
+      conceptId: 1
+    }
+  },
+  concepts: {
+    1: {
+      id: 1,
+      cardA: 1,
+      cardB: 2
+    }
+  },
+  cards: {
+    1: {
+      cardId: 1,
+      text: "büro"
+    },
+    2: {
+      cardId: 2,
+      text: "office"
+    }
+  }
+};
