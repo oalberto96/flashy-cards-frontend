45e059355693d2a9bf814e31d7e069fb01d68b1a
oalberto96 <ochoapublic@gmail.com>
2018-09-20 20:55:41 -0700

    Add card component

---
+++ src/features/Play/Card/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * Card
+ *
+ */
+
+import React from "react";
+import "./styles.css";
+
+const Card = () => {
+  return <div className="card-container">Word</div>;
+};
+export default Card;
--- src/features/Play/PlayCollection/index.js
+++ src/features/Play/PlayCollection/index.js
@@ -6,11 +6,21 @@
 
 import React from "react";
 import PropTypes from "prop-types";
+import Card from "../Card";
+import "./styles.css";
 
 const PlayCollection = ({ collectionId }) => {
   return (
-    <div className="container">
-      <div className="row">Hello im a Component of list {collectionId}</div>
+    <div className="play-collection-container">
+      <div className="container height-100-relative">
+        <div className="slider row height-100-relative">
+          <div className="col-1 col-md-2">{"<"}</div>
+          <div className="col-10 col-md-8">
+            <Card />
+          </div>
+          <div className="col-1 col-md-2">{">"}</div>
+        </div>
+      </div>
     </div>
   );
 };
