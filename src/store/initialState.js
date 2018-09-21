d0efb4652ea2322d8a1cdfee4f801c2f319d4e36
oalberto96 <ochoapublic@gmail.com>
2018-09-20 18:09:22 -0700

    Change state structure

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -4,24 +4,37 @@ export default {
       listId: 1,
       name: "name",
       description: "little description",
-      conceptId: 1
-    }
-  ],
-  concepts: [
-    {
-      id: 1,
-      cardA: 1,
-      cardB: 2
-    }
-  ],
-  cards: [
-    {
-      cardId: 1,
-      text: "büro"
+      concepts: [
+        {
+          conceptId: 1,
+          cardA: {
+            cardId: 1,
+            text: "büro"
+          },
+          cardB: {
+            cardId: 2,
+            text: "office"
+          }
+        }
+      ]
     },
     {
-      cardId: 2,
-      text: "office"
+      listId: 2,
+      name: "other name",
+      description: "little description",
+      concepts: [
+        {
+          conceptId: 1,
+          cardA: {
+            cardId: 3,
+            text: "flughafen"
+          },
+          cardB: {
+            cardId: 4,
+            text: "airport"
+          }
+        }
+      ]
     }
   ]
 };
