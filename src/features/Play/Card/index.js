2b8b261fdaf7ea5cdfb7819429dd599292b21282
oalberto96 <ochoapublic@gmail.com>
2018-09-21 19:27:25 -0700

    Flip card and change the value

--- src/features/Play/Card/index.js
+++ src/features/Play/Card/index.js
@@ -8,10 +8,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./styles.css";
 
-const Card = ({ cardText }) => {
-  return <div className="card-container">{cardText}</div>;
+const Card = ({ cardText, flipCard }) => {
+  return (
+    <div className="card-container" onClick={flipCard}>
+      {cardText}
+    </div>
+  );
 };
 Card.propTypes = {
-  cardText: PropTypes.string.isRequired
+  cardText: PropTypes.string.isRequired,
+  flipCard: PropTypes.func.isRequired
 };
 export default Card;
--- src/features/Play/PlayConcept/index.js
+++ src/features/Play/PlayConcept/index.js
@@ -9,21 +9,39 @@ import PropTypes from "prop-types";
 import Card from "../Card";
 import "./styles.css";
 
-const PlayConcept = ({ cardA, CardB }) => {
-  return (
-    <div className="play-lesson-container">
-      <div className="container height-100-relative">
-        <div className="slider row height-100-relative">
-          <div className="col-1 col-md-2">{"<"}</div>
-          <div className="col-10 col-md-8">
-            <Card cardText={cardA.text} />
+class PlayConcept extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { cardFlipped: false };
+  }
+
+  flipCard = () => {
+    this.setState({ cardFlipped: !this.state.cardFlipped });
+  };
+
+  render() {
+    return (
+      <div className="play-lesson-container">
+        <div className="container height-100-relative">
+          <div className="slider row height-100-relative">
+            <div className="col-1 col-md-2">{"<"}</div>
+            <div className="col-10 col-md-8">
+              <Card
+                flipCard={this.flipCard}
+                cardText={
+                  this.state.cardFlipped
+                    ? this.props.cardA.text
+                    : this.props.cardB.text
+                }
+              />
+            </div>
+            <div className="col-1 col-md-2">{">"}</div>
           </div>
-          <div className="col-1 col-md-2">{">"}</div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 PlayConcept.propTypes = {
   cardA: PropTypes.object.isRequired,
