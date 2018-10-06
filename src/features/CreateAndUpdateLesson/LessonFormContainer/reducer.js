f2b940f2f95ec7d63e461fc4828e23a8480c2cfe
oalberto96 <ochoapublic@gmail.com>
2018-10-05 17:56:32 -0700

    Fix newLesson mutability at REQUEST_LESSON_TO_EDIT_SUCCESS

--- src/features/CreateAndUpdateLesson/LessonFormContainer/reducer.js
+++ src/features/CreateAndUpdateLesson/LessonFormContainer/reducer.js
@@ -5,84 +5,90 @@
  */
 
 import {
-	ADD_CONCEPT_TO_NEW_LESSON,
-	SET_CARD_TEXT,
-	CHANGE_NEW_LESSON_NAME,
-	CHANGE_NEW_LESSON_DESCRIPTION,
-	SAVE_NEW_LESSON,
-	SET_CARD_IMAGE,
-	REQUEST_LESSON_TO_EDIT_SUCCESS
+  ADD_CONCEPT_TO_NEW_LESSON,
+  SET_CARD_TEXT,
+  CHANGE_NEW_LESSON_NAME,
+  CHANGE_NEW_LESSON_DESCRIPTION,
+  SAVE_NEW_LESSON,
+  SET_CARD_IMAGE,
+  REQUEST_LESSON_TO_EDIT_SUCCESS
 } from "./constants";
 
 const initialState = {
-	lessonName: "",
-	lessonDescription: "",
-	concepts: []
+  lessonName: "",
+  lessonDescription: "",
+  concepts: []
 };
 
 function LessonFormContainerReducer(state = initialState, action) {
-	switch (action.type) {
-		case ADD_CONCEPT_TO_NEW_LESSON:
-			let conceptsCopy = state.concepts.slice();
-			let lastConceptAdded = conceptsCopy[conceptsCopy.length - 1];
-			conceptsCopy.push({
-				conceptId:
-					lastConceptAdded !== undefined ? lastConceptAdded.conceptId + 1 : 0,
-				cardA: { text: "" },
-				cardB: { text: "" }
-			});
-			let newLesson = { concepts: conceptsCopy };
-			return { ...state, ...newLesson };
-		case SET_CARD_TEXT:
-			let cardText = action.payload.cardText;
-			conceptsCopy = state.concepts.map(concept => {
-				if (concept.conceptId === action.payload.conceptId) {
-					action.payload.card === "A"
-						? (concept.cardA = { ...concept.cardA, text: cardText })
-						: (concept.cardB = { ...concept.cardB, text: cardText });
-					return { ...concept };
-				}
-				return concept;
-			});
-			return { ...state };
-		case SET_CARD_IMAGE:
-			const cardImage = action.payload.cardImage;
-			conceptsCopy = state.concepts.map(concept => {
-				if (concept.conceptId === action.payload.conceptId) {
-					action.payload.card === "A"
-						? (concept.cardA = {
-								...concept.cardA,
-								media: {
-									type: "IMAGE",
-									source: cardImage
-								}
-						  })
-						: (concept.cardB = {
-								...concept.cardA,
-								media: {
-									type: "IMAGE",
-									source: cardImage
-								}
-						  });
-				}
-				return concept;
-			});
-			return { ...state, concepts: conceptsCopy };
-		case CHANGE_NEW_LESSON_NAME:
-			return { ...state, lessonName: action.payload.newLessonName };
-		case CHANGE_NEW_LESSON_DESCRIPTION:
-			return {
-				...state,
-				lessonDescription: action.payload.newLessonDescription
-			};
-		case SAVE_NEW_LESSON:
-			return initialState;
-		case REQUEST_LESSON_TO_EDIT_SUCCESS:
-			const lesson = { ...action.payload.lesson };
-			return lesson;
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case ADD_CONCEPT_TO_NEW_LESSON:
+      let conceptsCopy = state.concepts.slice();
+      let lastConceptAdded = conceptsCopy[conceptsCopy.length - 1];
+      conceptsCopy.push({
+        conceptId:
+          lastConceptAdded !== undefined ? lastConceptAdded.conceptId + 1 : 0,
+        cardA: { text: "" },
+        cardB: { text: "" }
+      });
+      let newLesson = { concepts: conceptsCopy };
+      return { ...state, ...newLesson };
+    case SET_CARD_TEXT:
+      let cardText = action.payload.cardText;
+      conceptsCopy = state.concepts.map(concept => {
+        if (concept.conceptId === action.payload.conceptId) {
+          action.payload.card === "A"
+            ? (concept.cardA = { ...concept.cardA, text: cardText })
+            : (concept.cardB = { ...concept.cardB, text: cardText });
+          return { ...concept };
+        }
+        return concept;
+      });
+      return { ...state };
+    case SET_CARD_IMAGE:
+      const cardImage = action.payload.cardImage;
+      conceptsCopy = state.concepts.map(concept => {
+        if (concept.conceptId === action.payload.conceptId) {
+          action.payload.card === "A"
+            ? (concept.cardA = {
+                ...concept.cardA,
+                media: {
+                  type: "IMAGE",
+                  source: cardImage
+                }
+              })
+            : (concept.cardB = {
+                ...concept.cardA,
+                media: {
+                  type: "IMAGE",
+                  source: cardImage
+                }
+              });
+        }
+        return concept;
+      });
+      return { ...state, concepts: conceptsCopy };
+    case CHANGE_NEW_LESSON_NAME:
+      return { ...state, lessonName: action.payload.newLessonName };
+    case CHANGE_NEW_LESSON_DESCRIPTION:
+      return {
+        ...state,
+        lessonDescription: action.payload.newLessonDescription
+      };
+    case SAVE_NEW_LESSON:
+      return initialState;
+    case REQUEST_LESSON_TO_EDIT_SUCCESS:
+      console.log(action.payload.lesson);
+      const lesson = {
+        ...action.payload.lesson,
+        concepts: action.payload.lesson.concepts.map(concept => ({
+          ...concept
+        }))
+      };
+      return lesson;
+    default:
+      return state;
+  }
 }
 
 export default LessonFormContainerReducer;
