0e715c0c34bb691ab04d64cf64e81ba7aa41aad9
oalberto96 <ochoapublic@gmail.com>
2019-05-01 11:13:43 -0700

    Show lesson menu options

--- src/features/LessonMenu/LessonMenu/index.js
+++ src/features/LessonMenu/LessonMenu/index.js
@@ -1,4 +1,10 @@
 import React from "react";
+import MenuOption from "../MenuOption";
+import * as styles from "./style.module.css";
+import studyIcon from "../../../img/icons/description.svg";
+import playIcon from "../../../img/icons/play_circle.svg";
+import editIcon from "../../../img/icons/edit.svg";
+import scoreIcon from "../../../img/icons/bar_chart.svg";
 
 const LessonMenu = () => {
   return (
@@ -6,13 +12,15 @@ const LessonMenu = () => {
       <div>Title</div>
       <p>Description</p>
 
+      <h2>What would you like to do?</h2>
+
       <div className="row">
-        <div className="col">Hallow</div>
-        <div className="col">dawd</div>
+        <MenuOption title="Study" icon={studyIcon} />
+        <MenuOption title="Play" icon={playIcon} />
       </div>
       <div className="row">
-        <div className="col">Hallow</div>
-        <div className="col">dawd</div>
+        <MenuOption title="Edit" icon={editIcon} />
+        <MenuOption title="Score" icon={scoreIcon} />
       </div>
     </div>
   );
--- src/features/LessonMenu/MenuOption/index.js
+++ src/features/LessonMenu/MenuOption/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import * as styles from "./style.module.css";
 
-const MenuOption = () => {
-  return asd;
+const MenuOption = ({ title, icon }) => {
+  return (
+    <div className="col">
+      <div className={styles.container}>
+        <img src={icon} className={`img-fluid ${styles.icon}`} />
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default MenuOption;
