6bf39ce5881e29608d3ecc14c18000c7ed79b6d5
oalberto96 <ochoapublic@gmail.com>
2019-09-14 14:07:17 -0700

    fix login positiion and labels

--- src/features/Authentication/Login/index.js
+++ src/features/Authentication/Login/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import TextInput from "../../../components/common/TextInput";
 import ErrorMessage from "../../../components/common/ErrorMessage";
-import './style.module.css';
+import style from "./style.module.css";
 
 class Login extends React.Component {
   state = {
@@ -17,42 +17,42 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.props.error && <ErrorMessage {...this.props.error} />}
-        <div className="form-group">
+      <div className="row align-items-center justify-content-center">
+        <form className={`${style.loginWrapper} col-lg-4 col-11 col-sm-11`}>
+          {this.props.error && <ErrorMessage {...this.props.error} />}
           <h1>Login</h1>
-          <TextInput
-            inputClass="form-control"
-            label="Email"
-            inputId="username"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <TextInput
-            className="Password"
-            inputClass="form-control"
-            label="Password"
-            inputId="password"
-            inputType="password"
-            onChange={this.handleChange}
-          />
-        </div>
-        <button
-          id="login-button"
-          className="btn btn-primary"
-          onClick={() =>
-            this.props.onClick(this.state.username, this.state.password)
-          }
-        >
-          Login
-        </button>
-        <Link className="btn btn-outline-info" to="/signup">
-          Sign Up
-        </Link>
 
-        
-
+          <div className="form-group">
+            <TextInput
+              inputClassName="form-control"
+              label="Email"
+              inputId="username"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <TextInput
+              className="Password"
+              inputClassName="form-control"
+              label="Password"
+              inputId="password"
+              inputType="password"
+              onChange={this.handleChange}
+            />
+          </div>
+          <button
+            id="login-button"
+            className="btn btn-primary"
+            onClick={() =>
+              this.props.onClick(this.state.username, this.state.password)
+            }
+          >
+            Login
+          </button>
+          <Link className="btn btn-outline-info" to="/signup">
+            Sign Up
+          </Link>
+        </form>
       </div>
     );
   }
